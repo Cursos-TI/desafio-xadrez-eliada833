ebfd45e66947cd93b13cd3ef1581477c050c4758
MARISA ILIVEIRA <marisafideles2009@hotmail.com>
2026-06-12 14:07:07 -0300

    Atualizando N2-Bispo usando while

--- n2_desafio.c
+++ n2_desafio.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Mover o Bispo 5 casas na diagonal Cima-Direita
-    for (int i = 0; i < 5; i++) {
+    // Mover o Bispo 5 casas na diagonal Cima-Direita usando WHILE
+    int i = 0; // Contador inicia em 0
+    
+    while (i < 5) { // Enquanto i for menor que 5, repete
         printf("Cima, Direita\n"); // Imprime as duas direções da diagonal
+        i++; // Incrementa o contador - MUITO IMPORTANTE pra não dar loop infinito
     }
+    
     return 0;
 }
