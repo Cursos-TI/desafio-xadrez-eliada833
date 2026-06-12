4acba5755f9ec9f8b82ff951ff580a6e48bcbabe
MARISA ILIVEIRA <marisafideles2009@hotmail.com>
2026-06-12 13:59:50 -0300

    Desafio N2-Movimento do Bispo com for

---
+++ n2_desafio.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Mover o Bispo 5 casas na diagonal Cima-Direita
+    for (int i = 0; i < 5; i++) {
+        printf("Cima, Direita\n"); // Imprime as duas direções da diagonal
+    }
+    return 0;
+}
